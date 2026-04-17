cb085643b7e3f6c9ba7679a946bfd93497045dd3
Plorbus <projectplorbus@gmail.com>
2026-04-17 15:42:46 -0700

    worked on combat system
    didnt finish, but set up the draft for it

---
+++ fight.js
@@ -0,0 +1,7 @@
+function openInventory() {
+    document.getElementById("overlayInv").style.display = "flex";
+}
+
+function closeInventory() {
+    document.getElementById("overlayInv").style.display = "none";
+}
